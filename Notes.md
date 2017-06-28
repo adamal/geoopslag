da119413504807aabc96aa1db320ca1004475946
Adam Andersen Læssøe <aalasso@gmail.com>
2017-06-28 08:19:01 +0200

    trying to get ejerlavs kode and matrikkelnummer

---
+++ Notes.md
@@ -0,0 +1,2 @@
+# Geodata
+Et jordstykke er unikt bestemt med et Ejerlavsnummer _og_ et Matrikkelnummer.
--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 $(function() {
   $('#adresse').dawaautocomplete({
-    select: function(event, data) {
-      $('#adresse-choice').text(data.tekst);
+    select: function(event, autocompleteData) {
+      $.get(autocompleteData.href, function(adresseData){
+	      $('#adresse-choice').text(
+	      		"Ejerlav: " + adresseData.jordstykke.ejerlav.kode + ";   Matrikkelnummer: " + adresseData.jordstykke.matrikelnr
+	      	);
+      })
+      console.log(autocompleteData)
     }
   });
 });
